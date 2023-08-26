ec070ff63195e59c90fab5c5d3367ead2d480cd5
doojit <doojit@me.com>
2023-08-26 12:23:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""This is just a very simple authentication example.
+
+Please see the `OAuth2 example at FastAPI <https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/>`_  or
+use the great `Authlib package <https://docs.authlib.org/en/v0.13/client/starlette.html#using-fastapi>`_ to implement a classing real authentication system.
+Here we just demonstrate the NiceGUI integration.
+"""
+from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from nicegui import app, ui
+
+# Create users from 01 to 14 with password 999
+passwords = {f'{i:02}': '999' for i in range(1, 15)}
+
+@ui.page('/')
+def main_page() -> None:
+    if not app.storage.user.get('authenticated', False):
+        return RedirectResponse('/login')
+    with ui.column().classes('absolute-center items-center'):
+        ui.label(f'Hello {app.storage.user["username"]}!').classes('text-2xl')
+        ui.button(on_click=lambda: (app.storage.user.clear(), ui.open('/login')), icon='logout').props('outline round')
+templates = Jinja2Templates(directory="templates")
+
+
+
+@ui.page('/login')
+def login() -> None:
+    def try_login() -> None:  # local function to avoid passing username and password as arguments
+        if passwords.get(username.value) == password.value:
+            app.storage.user.update({'username': username.value, 'authenticated': True})
+            ui.open('/home')
+        else:
+            ui.notify('Wrong username or password', color='negative')
+
+    if app.storage.user.get('authenticated', False):
+        return RedirectResponse('/')
+    with ui.card().classes('absolute-center'):
+        ui.label('Selesy Coastguard').classes('text-xl mb-4')  # Added text above login details
+        username = ui.input('Username').on('keydown.enter', try_login)
+        password = ui.input('Password', password=True, password_toggle_button=True).on('keydown.enter', try_login)
+        ui.button('Log in', on_click=try_login)
+
+
+@ui.page('/home')
+def home_page() -> None:
+    with ui.row().classes('w-full items-center'):
+        result = ui.label().classes('mr-auto')
+        with ui.button(icon='menu'):
+            with ui.menu() as menu:
+                ui.menu_item('Menu item 1', lambda: result.set_text('Selected item 1'))
+                ui.menu_item('Menu item 2', lambda: result.set_text('Selected item 2'))
+                ui.menu_item('Menu item 3 (keep open)',
+                             lambda: result.set_text('Selected item 3'), auto_close=False)
+                ui.separator()
+                ui.menu_item('Close', on_click=menu.close)
+
+ui.run(storage_secret='THIS_NEEDS_TO_BE_CHANGED')
+
+ui.run(storage_secret='THIS_NEEDS_TO_BE_CHANGED')
